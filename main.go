e24c59af032b3c88c323c17aba0ef246009a9a8e
Abduvohid1177 <vohidjon020306@gmail.com>
2023-11-17 17:47:48 +0500

    Uyga vazifa bajarildi

--- main.go
+++ main.go
@@ -4,8 +4,51 @@ import "fmt"
 
 func main() {
 
-	a := 55
-	b := 50
-	fmt.Println(a + b)
+	/* { fmt.Println("1-misol")   }
+	var a int
+	fmt.Print("3 xonali sonni kiriting: " ); fmt.Scan(&a)
+	fmt.Println((a/100)*100 + (a%10)*10 + (a/10)%10)
+	*/
+
+	/* { fmt.Println("2-misol")   }
+	var a int
+	fmt.Print("4 xonali sonni kiriting: " ); fmt.Scan(&a)
+	fmt.Println((a/100)%10) */
+
+	/* { fmt.Println("3-misol")   }
+	var a int
+	fmt.Print("4 xonali sonni kiriting: " ); fmt.Scan(&a)
+	fmt.Println(a/1000) */
+
+	/* { fmt.Println("4-misol")   }
+	var a int
+	fmt.Print("sekuntni kiriting: " ); fmt.Scan(&a)
+	fmt.Println("To`la minut: ", a/60) */
+
+	/* { fmt.Println("5-misol")   }
+	var a int
+	fmt.Print("sekuntni kiriting: " ); fmt.Scan(&a)
+	fmt.Println("To`la soat: ", a/3600) */
+
+	/* { fmt.Println("6-misol")   }
+	var a int
+	fmt.Print("sekuntni kiriting: " ); fmt.Scan(&a)
+	fmt.Println("To`la minut: ", a/60, " Sekunt: ", a%60) */
+
+	/* { fmt.Println("7-misol")   }
+	var a int
+	fmt.Print("sekuntni kiriting: " ); fmt.Scan(&a)
+	fmt.Println("To`la soat: ", a/3600, " Sekunt: ", a%60) */
+
+	/* { fmt.Println("8-misol")   }
+	var a int
+	fmt.Print("sekuntni kiriting: " ); fmt.Scan(&a)
+	fmt.Println("To`la soat: ", a/3600, "To`la minut: ", a%3600/60, " Sekunt: ", a%60) */
+
+	{ fmt.Println("9-misol")   }
+	var a int
+	fmt.Print("4 xonali sonni kiriting: " ); fmt.Scan(&a)
+	fmt.Println(a/1000)
+
 
 }
